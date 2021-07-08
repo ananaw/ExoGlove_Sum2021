7bc18eebd224e3c2ee2a55af5fab92454910a32f
ananaw <61036678+ananaw@users.noreply.github.com>
2021-07-07 18:45:41 -0700

    Update 2000DualMotorEncoder.ino

--- Test Codes/2000DualMotorEncoder.ino
+++ Test Codes/2000DualMotorEncoder.ino
@@ -9,63 +9,110 @@ by Ananda Aw
 
 // include the Servo library
 #include "ESP32Servo.h"
+#include "esp_adc_cal.h"
+#include "movingAvg.h"    // included J. Christensen's library github: https://github.com/JChristensen/movingAvg
 
-Servo myServo;  // create a servo object
+Servo myServo;  // Servo that controls thumb
+movingAvg avgPot1(10); // create moving average object 
+movingAvg avgPot2(10); 
 
-int const potPin1 = 13; // analog pin used to connect the potentiometer
-int const potPin2 = 21; // analog pin used to connect the potentiometer
-int potVal1;  // variable to read the value from the analog pin
-int potVal2;  // variable to read the value from the analog pin
-int pot1Voltage, pot2Voltage, angle1, anglesec, angle2;   // variable to hold the angle1for the servo motor (500-2500)usec
+#define AN_Pot1    34   // Thumb pot pin for position feedback
+#define AN_Pot2    35   // Thumb pot pin for position feedback
+#define FILTER_LEN 20   // length of 20 points, longer it is, smoother filter but takes longer time
+
+//uint32_t AN_Pot1_Buffer[FILTER_LEN] = {0}; // empty buffer
+//int AN_Pot1_i = 0;                         // buffer index
+
+int potVal1, potVal2; // variable to read the value from the analog pin
+int Pot1_Result = 0; int Pot2_Result = 0;
+float Pot1Final = 0.0; 
+float Pot2Final = 0.0;
+float pot1Voltage, pot2Voltage, angle1, anglesec, angle2;   // variable to hold the angle1 for the servo motor (500-2500)usec
 
 void setup() {
-  // Allow allocation of all timers
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3); 
+  Serial.begin(115200); // open a serial connection to your computer
+  delay(1000);          // allow some time to set up serial monitor
   
-  // Setup ADC
-  analogReadResolution(11); // set resolution to 11 bits (0-2047)
-  analogSetAttenuation(13||21, ADC_6db); // sets 6dB attenuation for 4,13,14,16 pins: 6dB range = 0-2.2V
+  analogSetWidth(12);
+  analogSetAttenuation(ADC_11db);
   // According to results provided by a problem, there's an offset of 0.1V and saturation at 1.85V. 
   // Link for the test results: https://esp32.com/viewtopic.php?f=19&t=2881&p=13739#p13739
   
-  // attaches the servo on pn 25 to the servo object, set min & max us
-  myServo.attach(25); // attaches the servo on pin 25 to the servo object
-  Serial.begin(115200); // open a serial connection to your computer
+  // attaches the servo on pin 14 to the servo object
+  myServo.attach(14); // attaches the servo on pin 14 to the servo object
+  avgPot1.begin();    // initializes the object
+  avgPot2.begin();  
 }
 
 void loop() {
-  //an  gle1= 500;
-  potVal1 = analogRead(potPin1); // read the value of the potentiometer
-  potVal2 = analogRead(potPin2); // read the value of the potentiometer
-  pot1Voltage = 3.3*potVal1/2047; // convert into voltage;
-  pot2Voltage = 3.3*potVal2/2047;
+  potVal1 = analogRead(AN_Pot1); // read the value of the potentiometer
+  int avg1 = avgPot1.reading(potVal1);
+  pot1Voltage = readADC_Cal(avg1);     // calibrate it.
+  Pot1Final = Voltage_Cal(pot1Voltage);  // Trendline equation from data collected comparing measurements from DMM (V) Vs ESP32 (V)
   
+  potVal2 = analogRead(AN_Pot2); // read the value of the potentiometer
+  //Pot2_Result = readADC2_Avg(potVal2); // go through filtering
+  int avg2 = avgPot2.reading(potVal2);
+  pot2Voltage = readADC_Cal(avg2);     // calibrate it.
+  //pot2Voltage = Pot2_Result*3.285/4095; // convert to voltage
+  Pot2Final = Voltage_Cal(pot2Voltage); 
+
   // scale the numbers from the pot
-  angle1= map(potVal1, 95, 1145, 0, 300); // the values 93 and 1148 came from converting linear range (0.15-1.85V) of ADC 
-  // scale the numbers from the pot
-  angle2= map(potVal2, 95, 1145, 300, 0);
+  angle1 = map(Pot1Final, 0.96, 3.07, 0, 300); // the values 0.96V and 3.07V came from linear range measured of ADC 
+  angle2 = map(Pot2Final, 0.96, 3.07, 300, 0); 
   // map degrees into duty cycles (in microsec)
-  anglesec = map(potVal1, 95, 1145, 500, 2500);
+  anglesec = map(Pot1Final, 0.96, 3.07, 500, 2500);
   
+  Serial.print(potVal1); 
   // print out the value to the Serial Monitor
-  Serial.print("potVal1: "); Serial.print(potVal1);
-  // print out the value to the Serial Monitor
-  Serial.print(", potVal2: "); Serial.print(potVal2);
+  Serial.print(", Pot1Voltage: "); Serial.print(Pot1Final);
   // print out the angle set for the servo motor
-  Serial.print(", angle1: "); Serial.print(angle1);
+  Serial.print(", angle1: "); Serial.print(angle1); Serial.print(", ");
+  Serial.print(potVal2);
+  // print out the value to the Serial Monitor
+  Serial.print(", Pot2Voltage: "); Serial.print(Pot2Final);
+  
   // print out the measured angle of the servo motor
-  Serial.print(", angle2: "); Serial.print(angle2);
-   // print out the angle set for the servo motor
-  Serial.print(", Voltage1: "); Serial.print(pot1Voltage);
-   // print out the angle set for the servo motor
-  Serial.print(", Voltage2: "); Serial.println(pot2Voltage);
+  Serial.print(", angle2: "); Serial.println(angle2);
 
   // set the servo position
   myServo.writeMicroseconds(anglesec);
   // wait for the servo to get there
   delay(150); 
 
+}
+
+uint32_t readADC_Cal(int ADC_Raw)  // ADC calibration (returns in mV)
+{
+  esp_adc_cal_characteristics_t adc_chars;
+
+  // ADC1, 11dB (0-4095), 12 bits, default Vref = 1.1V
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars); // address of adc_chars
+  return(esp_adc_cal_raw_to_voltage(ADC_Raw, &adc_chars));
+}
+/*
+uint32_t readADC_Avg(uint32_t arr[], int ADC_Raw) //moving average filter
+{
+  int i = 0;
+  uint32_t Sum = 0;
+  uint32_t AN_Pot1_Buffer[FILTER_LEN] = {0}; // empty buffer
+  int AN_Pot1_i = 0; 
+  
+  AN_Pot1_Buffer[AN_Pot1_i++] = ADC_Raw;
+  if(AN_Pot1_i == FILTER_LEN) // if buffer index full, then reset index
+  {
+    AN_Pot1_i = 0;
+  }
+  for(i=0; i<FILTER_LEN; i++)
+  {
+    Sum += AN_Pot1_Buffer[i];
+    arr[i] = AN_Pot1_Buffer[i];
+  }
+  return (Sum/FILTER_LEN);
+}*/
+
+float Voltage_Cal(float FilteredVolt) // takes float type input (in mV), if use my own conversion, then (V)
+{
+  float FinalVolt = 0;
+  return FinalVolt = 0.9955*FilteredVolt/1000 - 0.0535; //trendline equation obtained from data collected comparing measurements from DMM (V) Vs ESP32 (V) 
 }
